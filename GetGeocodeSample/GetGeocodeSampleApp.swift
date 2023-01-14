b3e8220de564dc3dc1b512e642ba6691c8c08966
Haruki Hanagata <haruzo28@icloud.com>
2023-01-14 09:28:14 +0900

    Initial Commit

---
+++ GetGeocodeSample/GetGeocodeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetGeocodeSampleApp.swift
+//  GetGeocodeSample
+//
+//  Created by 花形春輝 on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct GetGeocodeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
